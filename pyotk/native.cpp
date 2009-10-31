0cf42f2fc4b05dcf53d2edae4de912d9b2914cba
seppul <seppul@utu.fi>
2009-10-31 12:04:32 +0000

    Added support for combined stopping criteria to PyOTK.

--- pyotk/native.cpp
+++ pyotk/native.cpp
@@ -16,8 +16,10 @@
 #include <otkpp/localsolvers/native/PARTAN.h>
 #include <otkpp/localsolvers/native/linmin/Fletcher.h>
 #include <otkpp/localsolvers/native/linmin/MoreThuente.h>
+#include <otkpp/stopcrit/CompoundStoppingCriterion.h>
 #include <otkpp/stopcrit/FDistToMinTest.h>
 #include <otkpp/stopcrit/GradNormTest.h>
+#include <otkpp/stopcrit/MaxNumIterTest.h>
 #include <otkpp/stopcrit/StoppingCriterion.h>
 #include <otkpp/stopcrit/XDistToMinTest.h>
 #include <otkpp/testproblems/MGHTestFunction.h>
@@ -307,10 +309,14 @@ void init_solvers()
 
 void init_stop_crit()
 {
-  class_< StoppingCriterion, boost::noncopyable >("StoppingCriterion", no_init);
+  class_< StoppingCriterion, boost::noncopyable >("StoppingCriterion", no_init)
+    .def(self + self);
+  class_< CompoundStoppingCriterion, bases< StoppingCriterion > >("CompoundStoppingCriterion")
+    .def(self + self);
   class_< FDistToMinTest, bases< StoppingCriterion > >("FDistToMinTest", 
     init< double, double, bool >((arg("f_min"), arg("eps"), arg("relative") = false)));
   class_< GradNormTest, bases< StoppingCriterion > >("GradNormTest", init< double >(arg("eps")));
+  class_< MaxNumIterTest, bases< StoppingCriterion > >("MaxNumIterTest", init< int >(arg("n")));
   class_< XDistToMinTest, bases< StoppingCriterion > >("XDistToMinTest", 
     init< const vector< double > &, double, bool >((arg("x_min"), arg("eps"), arg("relative") = false)));
 }
